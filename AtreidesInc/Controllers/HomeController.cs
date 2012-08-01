3b7c51d3b2d2174caa8aa51d31a426e422cd52d0
Duncan Hill <duncsg1@outlook.com>
2012-08-01 09:37:58 -0500

    Initial Commit - Change of Home Page
    Changed the home page to say Atreides Inc in the 'Welcome to...'.

--- AtreidesInc/Controllers/HomeController.cs
+++ AtreidesInc/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace AtreidesInc.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to Atreides Inc!";
 
             return View();
         }
